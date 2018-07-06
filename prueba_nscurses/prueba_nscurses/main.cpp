4e786e4cb016b8b91a0d18c2a98ef554d369c431
Andrea <andrea.diaz.12@me.com>
2018-07-05 22:51:07 -0500

    mi parte totalmente terminada

--- prueba_nscurses/prueba_nscurses/main.cpp
+++ prueba_nscurses/prueba_nscurses/main.cpp
@@ -38,7 +38,7 @@ char *nextt[] = {"GUARDAR(F1)  "
                  "MENU(F3)",
 };
 
-char *nextt_2[] = {"GUARDAR(F1)  "
+const char *nextt_2[] = {"GUARDAR(F1)  "
     "ANTERIOR(F2)  "
     "MENU(F3)",
 };
@@ -52,9 +52,16 @@ const char *choiceslab[] = {
     "Heces",
 };
 
+const char *docs[] = { "Lista de Doctores", "Agregar Doctor"
+};
+
+
 int n_choices = sizeof(choices) / sizeof(char *);
+
 int n_nextt = sizeof(nextt) / sizeof(char *);
 int n_nextt_2 = sizeof(nextt_2) / sizeof(char *);
+
+int n_docs = sizeof(docs) / sizeof(char *);
 
 int n_choiceslab = sizeof(choices) / sizeof(char *);
 int n_nexttlab = sizeof(nextt) / sizeof(char *);
@@ -78,6 +85,29 @@ void menu (WINDOW *menu_win, int highlight)
         ++y;
     }
     wrefresh(menu_win);
+}
+
+//------- menu docs -----------
+void menu_docs (WINDOW *doctores_menu, int highlight)
+{
+    int x, y;
+    
+    x = 6;
+    y = 4;
+    box(doctores_menu, 0, 0);
+    for(int i = 0; i < n_docs; ++i)
+    {
+        if(highlight == i + 1) /* High light the present choice */
+        {
+            wattron(doctores_menu, A_REVERSE);
+            mvwprintw(doctores_menu, y, x, "%s", docs[i]);
+            wattroff(doctores_menu, A_REVERSE);
+        }
+        else
+            mvwprintw(doctores_menu, y, x, "%s", docs[i]);
+            ++y;
+        }
+    wrefresh(doctores_menu);
 }
 
 
@@ -114,7 +144,7 @@ void botones (WINDOW *barra, int highlight)
     box(barra, 0, 0);
     for(int i = 0; i < n_nextt; ++i)
     {
-        if(highlight == i + 1) /* High light the present choice */
+        if(highlight == i + 1)
         {
             //wattron(barra, A_REVERSE);
             mvwprintw(barra, y, x, "%s", nextt[i]);
@@ -131,7 +161,7 @@ void botones_2 (WINDOW *barra, int highlight)
 {
     int x, y;
     
-    x = 40;
+    x = 39;
     y = 1;
     box(barra, 0, 0);
     for(int i = 0; i < n_nextt_2; ++i)
@@ -150,12 +180,19 @@ void botones_2 (WINDOW *barra, int highlight)
 }
 
 
+
 //--------- intro. de variables -------
 //--------- variables de la ficha medica
 static FORM *ficha_medica_form;
 static FORM *ficha_medica_form_2;
-static WINDOW *ficha_medica, *ficha_medica_body, *ficha_medica_2, *ficha_medica_body_2, *barra, *barra_2, *doctores_win;
+static WINDOW *ficha_medica, *ficha_medica_body, *ficha_medica_2, *ficha_medica_body_2, *barra, *barra_2;
 static FIELD *ficha_medica_fields[16], *ficha_medica_fields_2[9];
+
+//--------- variables doctores -------------------
+static WINDOW *doctores, *doctores_2;
+static WINDOW *doctores_menu, *doctores_files_body, *doctores_files_body_2; //*doctores_files;
+static FORM *doctores_form, *doctores_form_2;
+static FIELD *doctores_fields[8], *doctores_fields_2[8];
 
 //--------- variables laboratorio ----------------
 static FORM *lab_form;
@@ -270,6 +307,80 @@ static void driver(int ch)
     
     wrefresh(ficha_medica);
 }
+void doctores_files_2(int g);
+static void driver_docs(int ch)
+{
+    int g = 0;
+    
+    switch (ch)
+    {
+        case KEY_F(1):
+        {
+            ofstream fm_file;
+            //fm_file.open("ficha_medica2.txt", ios::out);
+            fm_file.open("/Users/andreadiaz/projects/medDiary/prueba_nscurses/prueba_nscurses/doctores/doc1.txt", ios::out);
+            if (fm_file.is_open())
+            {
+                for (int i = 1; doctores_fields[i]; i = i + 2)
+                 {
+                 fm_file << trim_whitespaces(field_buffer(doctores_fields[i], 0));
+                 fm_file << "\n";
+                 }
+            }
+            break;
+        }
+            
+        case KEY_F(2):
+        {
+            doctores_form_2 = new_form(doctores_fields_2);
+            assert(doctores_fields_2 != NULL);
+            doctores_files_2(g);
+            break;
+        }
+            
+        case KEY_F(3):
+        {
+            doctores_form = new_form(doctores_fields);
+            assert(doctores_fields != NULL);
+            
+            main();
+            break;
+        }
+            
+        case KEY_DOWN:
+            form_driver(doctores_form, REQ_NEXT_FIELD);
+            form_driver(doctores_form, REQ_END_LINE);
+            break;
+            
+        case KEY_UP:
+            form_driver(doctores_form, REQ_PREV_FIELD);
+            form_driver(doctores_form, REQ_END_LINE);
+            break;
+            
+        case KEY_LEFT:
+            form_driver(doctores_form, REQ_PREV_CHAR);
+            break;
+            
+        case KEY_RIGHT:
+            form_driver(doctores_form, REQ_NEXT_CHAR);
+            break;
+            
+        case 127:
+            form_driver(doctores_form, REQ_DEL_PREV);
+            break;
+            
+        case KEY_DC:
+            form_driver(doctores_form, REQ_DEL_CHAR);
+            break;
+            
+        default:
+            form_driver(doctores_form, ch);
+            break;
+    }
+    
+    
+    wrefresh(doctores);
+}
 
 static void driver_2(int f)
 {
@@ -278,8 +389,9 @@ static void driver_2(int f)
     {
         case KEY_F(1):
         {
-            fstream fm_file;
-            fm_file.open("ficha_medica2.txt", ios::out);
+            ofstream fm_file;
+            //fm_file.open("ficha_medica2.txt", ios::out);
+            fm_file.open("/Users/andreadiaz/projects/medDiary/prueba_nscurses/prueba_nscurses/files/ficha_medica2.txt", ios::out);
             if (fm_file.is_open())
             {
                 fm_file << trim_whitespaces(field_buffer(ficha_medica_fields_2[1],0)) << "\n";
@@ -349,12 +461,83 @@ static void driver_2(int f)
     wrefresh(ficha_medica_2);
 }
 
-
+void doctores_win1(int g);
+static void driver_docs2(int ch)
+{
+    int g = 0;
+    
+    switch (ch)
+    {
+        case KEY_F(1):
+        {
+            ofstream fm_file;
+            //fm_file.open("ficha_medica2.txt", ios::out);
+            fm_file.open("/Users/andreadiaz/projects/medDiary/prueba_nscurses/prueba_nscurses/doctores/doc2.txt", ios::out);
+            if (fm_file.is_open())
+            {
+                for (int i = 1; doctores_fields_2[i]; i = i + 2)
+                {
+                    fm_file << trim_whitespaces(field_buffer(doctores_fields_2[i], 0));
+                    fm_file << "\n";
+                }
+            }
+            break;
+        }
+            
+        case KEY_F(2):
+        {
+            doctores_form = new_form(doctores_fields);
+            assert(ficha_medica_fields_2 != NULL);
+            doctores_win1(g);
+            break;
+        }
+            
+        case KEY_F(3):
+        {
+            doctores_form = new_form(doctores_fields);
+            assert(doctores_fields != NULL);
+            
+            main();
+            break;
+        }
+            
+        case KEY_DOWN:
+            form_driver(doctores_form_2, REQ_NEXT_FIELD);
+            form_driver(doctores_form_2, REQ_END_LINE);
+            break;
+            
+        case KEY_UP:
+            form_driver(doctores_form_2, REQ_PREV_FIELD);
+            form_driver(doctores_form_2, REQ_END_LINE);
+            break;
+            
+        case KEY_LEFT:
+            form_driver(doctores_form_2, REQ_PREV_CHAR);
+            break;
+            
+        case KEY_RIGHT:
+            form_driver(doctores_form_2, REQ_NEXT_CHAR);
+            break;
+            
+        case 127:
+            form_driver(doctores_form_2, REQ_DEL_PREV);
+            break;
+            
+        case KEY_DC:
+            form_driver(doctores_form_2, REQ_DEL_CHAR);
+            break;
+            
+        default:
+            form_driver(doctores_form_2, ch);
+            break;
+    }
+    
+    
+    wrefresh(doctores_2);
+}
 
 static void lab_driver_1(int f)
 {
-    int h = 0;
-    
     switch (f)
     {
         case KEY_F(1):
@@ -649,7 +832,7 @@ void form2 (int g)
     char new_fields[100];
     
     ifstream fm_read;
-    fm_read.open("ficha_medica2.txt");
+    fm_read.open("/Users/andreadiaz/projects/medDiary/prueba_nscurses/prueba_nscurses/files/en.lproj/ficha_medica2.txt");
     
     fm_read >> new_fields;
     set_field_buffer(ficha_medica_fields_2[1], 0, new_fields);
@@ -673,6 +856,265 @@ void form2 (int g)
     }
     delwin(ficha_medica_2);
     delwin(ficha_medica_body_2);
+}
+
+//----------- doctores pg. 1
+void docs_files(int g);
+
+void doctores_win1 (int g)
+{
+    //int c;
+    int highlight = 1;
+    int choice = 0;
+    
+    startx = (80 - WIDTH) / 2;
+    starty = (24 - HEIGHT) / 2;
+    
+    wrefresh(doctores);
+    clear();
+    refresh();
+    
+    doctores_files_body = newwin(24, 80, 0, 0);
+    assert(doctores_files_body != NULL);
+    box(doctores_files_body, 0, 0);
+    //doctores_menu = derwin(doctores, HEIGHT, WIDTH, starty, startx);
+    doctores = derwin(doctores_files_body, 16, 76, 3, 1);
+    
+    assert(doctores != NULL);
+    barra = derwin(doctores_files_body, 3, 78, 20, 1);
+    assert(barra != NULL);
+    box(barra, 0, 0);
+    botones(barra, highlight);
+    refresh();
+    mvwprintw(doctores_files_body, 1, 2, "Doctor");
+    
+    doctores_fields[0] = new_field(1, 20, 0, 1, 0, 0);
+    doctores_fields[1] = new_field(1, 40, 0, 21, 0, 0);
+    doctores_fields[2] = new_field(1, 20, 2, 1, 0, 0);
+    doctores_fields[3] = new_field(1, 40, 2, 21, 0, 0);
+    doctores_fields[4] = new_field(2, 20, 4, 1, 0, 0);
+    doctores_fields[5] = new_field(1, 40, 4, 21, 0, 0);
+    doctores_fields[6] = new_field(1, 20, 6, 1, 0, 0);
+    doctores_fields[7] = new_field(1, 40, 6, 21, 0, 0);
+    
+    assert(doctores_fields[0] != NULL &&
+           doctores_fields[1] != NULL &&
+           doctores_fields[2] != NULL &&
+           doctores_fields[3] != NULL &&
+           doctores_fields[4] != NULL &&
+           doctores_fields[5] != NULL &&
+           doctores_fields[6] != NULL &&
+           doctores_fields[7] != NULL);
+    
+    set_field_buffer(doctores_fields[0], 0, "Nombres");
+    set_field_buffer(doctores_fields[1], 0, "");
+    set_field_buffer(doctores_fields[2], 0, "Especialidad");
+    set_field_buffer(doctores_fields[3], 0, "");
+    set_field_buffer(doctores_fields[4], 0, "Clinica");
+    set_field_buffer(doctores_fields[5], 0, "");
+    set_field_buffer(doctores_fields[6], 0, "Telefono");
+    set_field_buffer(doctores_fields[7], 0, "");
+    
+    set_field_opts(doctores_fields[0], O_VISIBLE | O_PUBLIC | O_AUTOSKIP);
+    set_field_opts(doctores_fields[1], O_VISIBLE | O_PUBLIC | O_EDIT | O_ACTIVE);
+    set_field_opts(doctores_fields[2], O_VISIBLE | O_PUBLIC | O_AUTOSKIP);
+    set_field_opts(doctores_fields[3], O_VISIBLE | O_PUBLIC | O_EDIT | O_ACTIVE);
+    set_field_opts(doctores_fields[4], O_VISIBLE | O_PUBLIC | O_AUTOSKIP);
+    set_field_opts(doctores_fields[5], O_VISIBLE | O_PUBLIC | O_EDIT | O_ACTIVE);
+    set_field_opts(doctores_fields[6], O_VISIBLE | O_PUBLIC | O_AUTOSKIP);
+    set_field_opts(doctores_fields[7], O_VISIBLE | O_PUBLIC | O_EDIT | O_ACTIVE);
+    
+    set_field_back(doctores_fields[1], A_UNDERLINE);
+    set_field_back(doctores_fields[3], A_UNDERLINE);
+    set_field_back(doctores_fields[5], A_UNDERLINE);
+    set_field_back(doctores_fields[7], A_UNDERLINE);
+    
+    doctores_form = new_form(doctores_fields);
+    assert(doctores_form != NULL);
+    set_form_win(doctores_form, doctores);
+    set_form_sub(doctores_form, derwin(doctores, 18, 76, 1, 1));
+    post_form(doctores_form);
+    
+    refresh();
+    wrefresh(doctores_files_body);
+    wrefresh(doctores);
+    
+    char new_fields[100];
+    
+    ifstream fm_read;
+    fm_read.open("/Users/andreadiaz/projects/medDiary/prueba_nscurses/prueba_nscurses/doctores/doc1.txt");
+
+    for (int i = 1; i < 17; i = i + 2) {
+        fm_read >> new_fields;
+        set_field_buffer(doctores_fields[i], 0, new_fields);
+    }
+    
+    fm_read.close();
+    
+    while ((g = getch()))
+        driver_docs(g);
+    
+    unpost_form(doctores_form);
+    free_form(doctores_form);
+    for (int i = 0; doctores_fields[i]; i++)
+    {
+        free_field(doctores_fields[i]);
+    }
+    delwin(doctores);
+    delwin(doctores_files_body);
+    
+    
+    
+    keypad(doctores_menu, TRUE);
+    refresh();
+    menu_docs(doctores_menu, highlight);
+    
+    
+    /*while(1)
+    {    c = wgetch(doctores_menu);
+        switch(c)
+        {
+            case KEY_UP:
+                if(highlight == 1)
+                {
+                    highlight = n_docs;
+                }
+                
+                else
+                    --highlight;
+                break;
+                
+            case KEY_DOWN:
+                if(highlight == n_docs)
+                    highlight = 1;
+                else
+                    ++highlight;
+                break;
+                
+            case 10:
+            {
+                choice = highlight;
+                if (choice == 1)
+                {
+                    docs_files();
+                }
+            }
+                
+            default:
+                refresh();
+                break;
+        }
+        menu_docs(doctores_menu, highlight);
+        if(choice != 0)
+            break;
+    }*/
+    mvprintw(23, 0, "You chose choice %d with choice string %s\n", choice, docs[choice - 1]);
+    
+    
+}
+
+
+//----------- doctores pg. 2
+void doctores_files_2(int g)
+{
+    //int c;
+    int highlight = 1;
+    int choice = 0;
+    
+    startx = (80 - WIDTH) / 2;
+    starty = (24 - HEIGHT) / 2;
+    
+    wrefresh(doctores);
+    clear();
+    refresh();
+    
+    doctores_files_body_2 = newwin(24, 80, 0, 0);
+    assert(doctores_files_body_2 != NULL);
+    box(doctores_files_body_2, 0, 0);
+    doctores_2 = derwin(doctores_files_body, 16, 76, 3, 1);
+    
+    assert(doctores_2 != NULL);
+    barra_2 = derwin(doctores_files_body_2, 3, 78, 20, 1);
+    assert(barra_2 != NULL);
+    box(barra_2, 0, 0);
+    botones_2(barra_2, highlight);
+    refresh();
+    mvwprintw(doctores_files_body_2, 1, 2, "Doctor");
+    
+    doctores_fields_2[0] = new_field(1, 20, 0, 1, 0, 0);
+    doctores_fields_2[1] = new_field(1, 40, 0, 21, 0, 0);
+    doctores_fields_2[2] = new_field(1, 20, 2, 1, 0, 0);
+    doctores_fields_2[3] = new_field(1, 40, 2, 21, 0, 0);
+    doctores_fields_2[4] = new_field(2, 20, 4, 1, 0, 0);
+    doctores_fields_2[5] = new_field(1, 40, 4, 21, 0, 0);
+    doctores_fields_2[6] = new_field(1, 20, 6, 1, 0, 0);
+    doctores_fields_2[7] = new_field(1, 40, 6, 21, 0, 0);
+    
+    assert(doctores_fields_2[0] != NULL &&
+           doctores_fields_2[1] != NULL &&
+           doctores_fields_2[2] != NULL &&
+           doctores_fields_2[3] != NULL &&
+           doctores_fields_2[4] != NULL &&
+           doctores_fields_2[5] != NULL &&
+           doctores_fields_2[6] != NULL &&
+           doctores_fields_2[7] != NULL);
+    
+    set_field_buffer(doctores_fields_2[0], 0, "Nombres");
+    set_field_buffer(doctores_fields_2[1], 0, "");
+    set_field_buffer(doctores_fields_2[2], 0, "Especialidad");
+    set_field_buffer(doctores_fields_2[3], 0, "");
+    set_field_buffer(doctores_fields_2[4], 0, "Clinica");
+    set_field_buffer(doctores_fields_2[5], 0, "");
+    set_field_buffer(doctores_fields_2[6], 0, "Telefono");
+    set_field_buffer(doctores_fields_2[7], 0, "");
+    
+    set_field_opts(doctores_fields_2[0], O_VISIBLE | O_PUBLIC | O_AUTOSKIP);
+    set_field_opts(doctores_fields_2[1], O_VISIBLE | O_PUBLIC | O_EDIT | O_ACTIVE);
+    set_field_opts(doctores_fields_2[2], O_VISIBLE | O_PUBLIC | O_AUTOSKIP);
+    set_field_opts(doctores_fields_2[3], O_VISIBLE | O_PUBLIC | O_EDIT | O_ACTIVE);
+    set_field_opts(doctores_fields_2[4], O_VISIBLE | O_PUBLIC | O_AUTOSKIP);
+    set_field_opts(doctores_fields_2[5], O_VISIBLE | O_PUBLIC | O_EDIT | O_ACTIVE);
+    set_field_opts(doctores_fields_2[6], O_VISIBLE | O_PUBLIC | O_AUTOSKIP);
+    set_field_opts(doctores_fields_2[7], O_VISIBLE | O_PUBLIC | O_EDIT | O_ACTIVE);
+    
+    set_field_back(doctores_fields_2[1], A_UNDERLINE);
+    set_field_back(doctores_fields_2[3], A_UNDERLINE);
+    set_field_back(doctores_fields_2[5], A_UNDERLINE);
+    set_field_back(doctores_fields_2[7], A_UNDERLINE);
+    
+    doctores_form_2 = new_form(doctores_fields_2);
+    assert(doctores_form_2 != NULL);
+    set_form_win(doctores_form_2, doctores_2);
+    set_form_sub(doctores_form_2, derwin(doctores_2, 18, 76, 1, 1));
+    post_form(doctores_form_2);
+    
+    refresh();
+    wrefresh(doctores_files_body_2);
+    wrefresh(doctores_2);
+    
+    char new_fields[100];
+    
+    ifstream fm_read;
+    fm_read.open("/Users/andreadiaz/projects/medDiary/prueba_nscurses/prueba_nscurses/doctores/doc2.txt");
+    
+    for (int i = 1; i < 17; i = i + 2) {
+        fm_read >> new_fields;
+        set_field_buffer(doctores_fields_2[i], 0, new_fields);
+    }
+    
+    fm_read.close();
+    
+    while ((g = getch()))
+        driver_docs2(g);
+    
+    unpost_form(doctores_form_2);
+    free_form(doctores_form_2);
+    for (int i = 0; doctores_fields_2[i]; i++)
+    {
+        free_field(doctores_fields_2[i]);
+    }
+    delwin(doctores_2);
+    delwin(doctores_files_body_2);
 }
 
 
@@ -842,6 +1284,7 @@ int main()
     int choice = 0;
     int c, ch = 0;
     int a = 0;
+    int g = 0;
     
     //------ inicio del programa
     initscr();
@@ -909,20 +1352,10 @@ int main()
                 //------- Doctores
                 if (choice == 2)
                 {
-                    wrefresh(doctores_win);
-                    clear();
-                    refresh();
-                    doctores_win = newwin(24, 80, 0, 0);
-                    assert(doctores_win != NULL);
-                    
-                    refresh();
-                    mvwprintw(doctores_win, 1, 2, "Doctores");
-                    box(doctores_win, 0, 0);
-                    
-                    
-                    refresh();
-                    wrefresh(doctores_win);
-                    continue;
+                    doctores_form = new_form(doctores_fields);
+                    assert(doctores_fields != NULL);
+                    doctores_win1(g);
+                    break;
                 }
                 
                 //------- Laboratorios
@@ -936,7 +1369,7 @@ int main()
                 break;
         }
         menu(menu_win, highlight);
-        if(choice != 0)    /* User did a choice come out of the infinite loop */
+        if(choice != 0)
             break;
     }
 
